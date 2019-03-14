6b1a8aff06240774ad03f751f009aa3338fd7823
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-13 22:17:46 -0700

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Prudhvi Gadiraju on 3/13/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
