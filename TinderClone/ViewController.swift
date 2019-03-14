da0ac091cdf8e907ece9188c0120769512020928
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-13 23:36:47 -0700

    - Setup Layout

--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let topStackView = HomeTopNavStackView()
+    let middleView = UIView()
+    let bottomStackView = HomeBottomControlsStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupLayout()
     }
-
-
+    
+    fileprivate func setupLayout() {
+        let mainStackView = UIStackView(arrangedSubviews: [topStackView, middleView, bottomStackView])
+        mainStackView.axis = .vertical
+        
+        view.addSubview(mainStackView)
+        mainStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor)
+    }
 }
-
